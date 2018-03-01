0af9c303e3d012e9e52e0b3382e3cb7e5436dde3
hzwy23 <hzwy23@163.com>
2018-03-01 23:57:28 +0800

    update

--- src/main/java/com/wisrc/basic/BasicApplication.java
+++ src/main/java/com/wisrc/basic/BasicApplication.java
@@ -1,10 +1,8 @@
 package com.wisrc.basic;
 
-import io.servicecomb.springboot.starter.provider.EnableServiceComb;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -14,8 +12,6 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(
    basePackages = {"com"}
 )
-@EnableDiscoveryClient
-@EnableServiceComb
 public class BasicApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/wisrc/webapp/controller/Index.java
+++ src/main/java/com/wisrc/webapp/controller/Index.java
@@ -3,30 +3,27 @@ package com.wisrc.webapp.controller;
 
 import com.wisrc.webapp.entity.SysSecUser;
 import com.wisrc.webapp.service.UserInfoService;
-import io.servicecomb.provider.rest.common.RestSchema;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @Api(value = "应用首页控制器")
-@RestSchema(schemaId = "test")
 public class Index {
 
     @Autowired
     private UserInfoService userInfoService;
 
-    @ResponseBody
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ApiOperation(value = "首页", notes = "应用服务首页服务")
     public String index() {
-        return "hello world";
+        return "index";
     }
 
     @RequestMapping(value = "/user", method = RequestMethod.GET)
